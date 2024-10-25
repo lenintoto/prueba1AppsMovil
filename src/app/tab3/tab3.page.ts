66a664bf86f75284a9069fc0b785a928aafd58b8
lenintoto <lenincgg_aries1@hotmail.com>
2024-10-25 14:57:14 -0500

    Avance 3 Prueba 1

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-tab3',
@@ -6,7 +8,14 @@ import { Component } from '@angular/core';
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
-
-  constructor() {}
-
+  fechaini?: string;
+  fechafin?: string;
+  constructor(public navCtrl: NavController) {
+  }
+  calcular(){
+    var ini = moment(this.fechaini);
+    var fin = moment(this.fechafin);
+    var diff = fin.diff(ini,'days');
+    console.log(diff)
+  }
 }
